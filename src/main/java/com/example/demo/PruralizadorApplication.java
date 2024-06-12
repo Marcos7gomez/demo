ee72e1ac979db8b9f360ada6aa800dd2f156ca44
Marcos Gomez <marcoslg.gomez@gmail.com>
2024-06-12 11:51:40 -0300

    first

---
+++ src/main/java/com/example/demo/PruralizadorApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruralizadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruralizadorApplication.class, args);
+	}
+
+}
